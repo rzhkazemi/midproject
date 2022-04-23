0a4be86955a09999edb24943a86722ff34b3c73c
razieh <rzh.kzm@gmail.com>
2022-04-23 13:30:29 +0430

    one more object added to the class.

--- bk.py
+++ bk.py
@@ -4,8 +4,10 @@ class Book:
         self.author = author
         self.year = year
     def get_descriptive(self):
-        print(f"{self.title} written by {self.author} in {self.year}")
+        print(f"{self.title} written by {self.author} in {self.year}")        
         
 favorite_book = Book("kaleidar","Dolatabadi", 1946 )
 favorite_book.get_descriptive()
+favorite_book02 = Book("Desiree", "Selinco", 1891)
+favorite_book02.get_descriptive()
         
